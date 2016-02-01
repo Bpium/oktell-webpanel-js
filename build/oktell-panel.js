40a338640efa3a0efd13d2f6466709a550c87280
Ramil Garipov <ramillion@ya.ru>
2016-02-01 15:31:58 +0300

    fix hard dependencies from oktelljs and oktell-voice

--- build/oktell-panel.js
+++ build/oktell-panel.js
@@ -1,4 +1,4 @@
-/* Oktell-panel.js 0.4.0 http://js.oktell.ru/webpanel */
+/* Oktell-panel.js 0.4.1 http://js.oktell.ru/webpanel */
 
 /*! Copyright (c) 2013 Brandon Aaron (http://brandonaaron.net)
  * Licensed under the MIT License (LICENSE.txt).
